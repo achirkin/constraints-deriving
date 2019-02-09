a94100fa23735ee90a6ba7dacbf18eaf50b53c73
achirkin <achirkin@users.noreply.github.com>
2019-02-09 21:39:54 +0100

    Test on GHC 8.0.1+

--- README.md
+++ README.md
@@ -48,3 +48,4 @@ But the whole thing is executed at compile time, so there is no danger of failur
 	* [ ] Fix that `matchTys` in `matchInstance` does not match data family instances
   * [ ] Keep generated files from Spec folder during tests
   * [ ] Test if `Coercible basetype newtype` before adding DeriveAll instances
+  * [ ] Remove generated .hi and .o files from test/Spec
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,10 +1,12 @@
+{-# LANGUAGE CPP                 #-}
 module Main (main) where
 
-import Control.Monad (join)
 import DynFlags
 import GHC
 import GHC.Paths     (libdir)
+#if __GLASGOW_HASKELL__ >= 802
 import Util          (OverridingBool (..))
+#endif
 
 main :: IO ()
 main = do
@@ -18,18 +20,29 @@ main = do
             , optLevel    = 0
             , ways        = []
             , useUnicode  = False
+#if __GLASGOW_HASKELL__ >= 802
             , useColor    = Never
             , canUseColor = False
+#endif
             } `gopt_set` Opt_DoCoreLinting
               `gopt_unset` Opt_PrintUnicodeSyntax
+#if __GLASGOW_HASKELL__ >= 802
               `gopt_unset` Opt_DiagnosticsShowCaret
+#endif
               `gopt_set` Opt_ForceRecomp
               -- `dopt_set` Opt_D_dump_deriv
-          minusWall = join . map snd $ filter (("all"==) . fst) warningGroups
-          fgs = foldl wopt_set fgs' minusWall
+      (fgs, _, _) <- parseDynamicFlags fgs'
+        [ noLoc "-Wall"
+        , noLoc "-package ghc"
+        , noLoc "-package constraints-deriving"]
       _ <- setSessionDynFlags fgs
       target <- guessTarget "test/Spec/DeriveAll01.hs" Nothing
       setTargets [target]
+#if __GLASGOW_HASKELL__ < 802
+      parseImportDecl "import StaticFlags (initStaticOpts)"
+        >>= setContext . (:[]) . IIDecl
+      _ <- execStmt "initStaticOpts" execOptions
+#endif
       load LoadAllTargets
   case r of
     Succeeded -> putStrLn "Done!"
