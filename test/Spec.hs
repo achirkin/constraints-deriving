edbe6fa0c6f45db805458ecdfeca3d320af25230
achirkin <achirkin@users.noreply.github.com>
2019-02-18 21:11:48 +0100

    Refactored Spec a bit

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,49 +1,68 @@
-{-# LANGUAGE CPP                 #-}
+{-# LANGUAGE CPP         #-}
+{-# LANGUAGE QuasiQuotes #-}
 module Main (main) where
 
 import DynFlags
 import GHC
-import GHC.Paths     (libdir)
+import GHC.Paths (libdir)
+import Path
+import Path.IO
 #if __GLASGOW_HASKELL__ >= 802
-import Util          (OverridingBool (..))
+import Util (OverridingBool (..))
 #endif
+
+-- | Folder with test modules to be compiled
+specDir :: Path Rel Dir
+specDir = [reldir|test/Spec/|]
+
 
 main :: IO ()
 main = do
-  r <- defaultErrorHandler defaultFatalMessager defaultFlushOut $
-    runGhc (Just libdir) $ do
-      dflags <- getSessionDynFlags
-      let fgs' = dflags
-            { ghcMode     = OneShot
-            , ghcLink     = NoLink
-            , verbosity   = 1
-            , optLevel    = 0
-            , ways        = []
-            , useUnicode  = False
+  targetModules <- filter ((".hs" ==) . fileExtension) . snd <$> listDir specDir
+  withSystemTempDir "constraints-deriving-tests" $ \outDir -> do
+    r <- defaultErrorHandler defaultFatalMessager defaultFlushOut $
+      runGhc (Just libdir) $ do
+        dflags' <- makeSimpleAndFast <$> getSessionDynFlags
+        (dflags, _, _) <- parseDynamicFlags dflags'
+          [ noLoc "-Wall"
+          , noLoc "-package ghc"
+          , noLoc "-package constraints-deriving"
+          , noLoc $ "-outputdir " ++ toFilePath outDir]
+        _ <- setSessionDynFlags dflags
+        traverse (flip guessTarget Nothing . toFilePath) targetModules >>= setTargets
+        ghc800StaticFlagsFix
+        load LoadAllTargets
+    case r of
+      Succeeded -> putStrLn "Done!"
+      Failed    -> putStrLn "Oops!"
+
+
+makeSimpleAndFast :: DynFlags -> DynFlags
+makeSimpleAndFast flags = flags
+  { ghcMode     = OneShot
+  , ghcLink     = NoLink
+  , verbosity   = 1
+  , optLevel    = 0
+  , ways        = []
+  , useUnicode  = False
 #if __GLASGOW_HASKELL__ >= 802
-            , useColor    = Never
-            , canUseColor = False
+  , useColor    = Never
+  , canUseColor = False
 #endif
-            } `gopt_set` Opt_DoCoreLinting
-              `gopt_unset` Opt_PrintUnicodeSyntax
+  } `gopt_set` Opt_DoCoreLinting
+    `gopt_set` Opt_ForceRecomp
+    `gopt_unset` Opt_PrintUnicodeSyntax
 #if __GLASGOW_HASKELL__ >= 802
-              `gopt_unset` Opt_DiagnosticsShowCaret
+    `gopt_unset` Opt_DiagnosticsShowCaret
 #endif
-              `gopt_set` Opt_ForceRecomp
-              -- `dopt_set` Opt_D_dump_deriv
-      (fgs, _, _) <- parseDynamicFlags fgs'
-        [ noLoc "-Wall"
-        , noLoc "-package ghc"
-        , noLoc "-package constraints-deriving"]
-      _ <- setSessionDynFlags fgs
-      target <- guessTarget "test/Spec/DeriveAll01.hs" Nothing
-      setTargets [target]
-#if __GLASGOW_HASKELL__ < 802
-      parseImportDecl "import StaticFlags (initStaticOpts)"
-        >>= setContext . (:[]) . IIDecl
-      _ <- execStmt "initStaticOpts" execOptions
+
+ghc800StaticFlagsFix :: Ghc ()
+#if __GLASGOW_HASKELL__ >= 802
+ghc800StaticFlagsFix = return ()
+#else
+ghc800StaticFlagsFix = do
+  decl <- parseImportDecl "import StaticFlags (initStaticOpts)"
+  setContext [IIDecl decl]
+  _ <- execStmt "initStaticOpts" execOptions
+  return ()
 #endif
-      load LoadAllTargets
-  case r of
-    Succeeded -> putStrLn "Done!"
-    Failed    -> putStrLn "Oops!"
