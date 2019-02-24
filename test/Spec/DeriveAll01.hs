6091aecc5391530d574297a14664b790cf30746a
achirkin <achirkin@users.noreply.github.com>
2019-02-24 10:10:25 +0100

    Improving DeriveAll logic

--- test/Spec/DeriveAll01.hs
+++ test/Spec/DeriveAll01.hs
@@ -16,34 +16,9 @@ data instance FooFam Float Float = FooFloats Float Float
 data instance FooFam Float String = FooString Float String
   deriving Show
 
-
---{-# ANN type TestData DeriveAll #-}
 {-# ANN type TestNewtype1 DeriveAll #-}
-{-# ANN type TestNewtype2 DeriveAll #-}
---{-# ANN TestNewtype2C DeriveAll #-}
---{-# ANN TestNewtype2C DeriveAll #-}
-
--- data TestData a = TData a a Int
 newtype TestNewtype1 a b = TestNewtype1C (FooFam a b)
+
+{-# ANN type TestNewtype2 DeriveAll #-}
 newtype TestNewtype2 a b r = TestNewtype2C r
 type instance DeriveContext (TestNewtype2 a b r) = FooFam a b ~ r
---
---{-# ANN type TestNewtype3 DeriveAll #-}
---newtype TestNewtype3 a = TestNewtype3C a
----- Enumerate specific instances
---type instance DeriveContext (TestNewtype3 Bool) = ()
---type instance DeriveContext (TestNewtype3 Int)  = ()
---type instance DeriveContext (TestNewtype3 (Maybe a)) = ()
---
---
--- {-# ANN type Newclass DeriveAll #-}
--- class Newclass a where
---  hereAmI :: a
-
---{-# ANN type Properclass DeriveAll #-}
---class Properclass a where
---  p1 :: a
---  p2 :: (a, a)
---
---{-# ANN type TestTypeType DeriveAll #-}
---type TestTypeType t n = TestNewtype1 t n
